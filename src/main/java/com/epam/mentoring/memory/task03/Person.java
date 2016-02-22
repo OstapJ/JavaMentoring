31c4b74e7d8d7444d4ba2356c9c8c20c8e1157d4
Ievgen_Ostapenko <Ievgen.Ostapenko@thomsonreuters.com>
2016-02-22 23:57:43 +0300

    Pushed task 03

---
+++ src/main/java/com/epam/mentoring/memory/task03/Person.java
@@ -0,0 +1,21 @@
+package com.epam.mentoring.memory.task03;
+
+import org.kohsuke.randname.RandomNameGenerator;
+
+import java.util.Random;
+
+public class Person
+{
+	private int age;
+	private String name;
+
+	public Person()
+	{
+		this.age = new Random().nextInt(9);
+		this.name = new RandomNameGenerator().next();
+	}
+
+	@Override public String toString(){
+		return String.format("%s is %03d years old", name, age);
+	}
+}
